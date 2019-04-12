b7e551d0b7772fc17a3bc347c8cc1367fe8cf6d7
wangjian1 <wangjian1@iauto.com>
2019-04-12 17:52:30 +0800

    54

--- app/src/main/java/com/iauto/myapplication/fragment/Fragment3.java
+++ app/src/main/java/com/iauto/myapplication/fragment/Fragment3.java
@@ -21,12 +21,11 @@ public class Fragment3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout3, container, false);
         ListView listView = (ListView) view.findViewById(R.id.list);
-            if(hotelInfo.getLinkedList()!=null){
-                String[] array = (String[]) hotelInfo.getLinkedList().toArray();
-                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, array);
-                listView.setAdapter(arrayAdapter);
-
-            }else {
+            if(hotelInfo.getLinkedList()!=null) {
+                System.out.println(hotelInfo.getLinkedList().get(0));
+                System.out.println(hotelInfo.getLinkedList().size());
+            }
+            
                 String[] array = {"北京府右街宾馆", "连升商务酒店", "和家宾馆连锁(北京安贞医院店)", "北京顺义宾馆", "中油宾馆", "锦江之星酒店(北京广渠门店)", "汉庭酒店(北京前门大街店)",
                         "和家宾馆连锁(北京北四环店)", "汉庭酒店(北京西直门新店)", "锦江之星酒店(北京广安门店)", "IU酒店(北京西客站六里桥东地铁站店)", "北京军都大酒店(军都旅游度假村)", "巴比伦时尚酒店",
                         "北京角楼国际青年旅舍(天安门店)", "连升商务酒店", "和家宾馆连锁(北京安贞医院店)", "北京顺义宾馆", "中油宾馆", "锦江之星酒店(北京广渠门店)", "汉庭酒店(北京前门大街店)",
@@ -34,7 +33,7 @@ public class Fragment3 extends Fragment {
                         "北京角楼国际青年旅舍(天安门店)", "汉庭酒店(北京亚运村鸟巢店)", "锦江之星酒店(北京马家堡店)", "桔子酒店(北京劲松东店)", "飘HOME连锁酒店(酒仙桥店)", "贯通现代酒店(前门店)"};
                 ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, array);
                 listView.setAdapter(arrayAdapter);
-            }
+
         return view;
     }
 
